32032fcf6824c957920cded8da0f5e4a04a9993f
Karel Dohnal <karel.dohnal@gmail.com>
2022-01-02 10:35:58 +0100

    initial

---
+++ src/lib/components/Snackbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const Snackbar = () => {
+  return (
+    <div>
+      Ahoj
+    </div>
+  );
+};
+
+// Snackbar.propTypes = {
+//
+// };
+
+export default Snackbar;
